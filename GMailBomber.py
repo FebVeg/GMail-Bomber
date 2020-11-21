b4c58ff6e1d448fc81840e40f679675f94617c68
FebVeg <feb19958@gmail.com>
2020-11-21 11:35:38 +0100

    Update GMailBomber.py

--- GMailBomber.py
+++ GMailBomber.py
@@ -1,60 +1,60 @@
+# -*- coding: utf-8 -*-
+
 from smtplib import SMTP
 from getpass import getpass
 from time import sleep, strftime
 
-def log(log):
-    print(f"[{strftime('%H:%M:%S')}] {log}")
+print("\n\tGMail Spammer Bot - FebVeg\n")
 
-log("Script started!")
+print("Server".ljust(15)+": First manually configuration...")
+username = input("Username".ljust(15)+": ")
+password = getpass("Password".ljust(15)+": ")
+sender   = input("Sender".ljust(15)+": ")
+subject  = input("Subject".ljust(15)+": ") + "\n\n"
+content  = input("Message".ljust(15)+": ")
+wait_sec = input("Wait seconds".ljust(15)+": ")
+times    = input("Times".ljust(15)+": ")
+
+if times == "":
+    times = 1
+if wait_sec == "":
+    wait_sec = 1
+
+message = subject+content
+
 try:
-    log("Creating the SMTP object ...")
+    print("Server".ljust(15)+": Connecting to server...")
     server = SMTP("smtp.gmail.com", 587)
-    log("Getting hello from SMPT server...")
-    log(str(server.ehlo()))
-    log("Starting the SSL tunnel ...")
+    print("Server".ljust(15)+f": {server.ehlo()}")
+    print("Server".ljust(15)+": Enabling SSL...")
     server.starttls()
-    log("Make the login [the password will not be shown] ...")
-    while True:
-        try:
-            usergmail = input("\tUsername: ")
-            passgmail = getpass("\tPassword: ")
-            server.login(usergmail, passgmail)
-            log("Login succeded!")
-            break
-        except Exception as Err:
-            log(f"Error 0x0: {str(Err)}")
+    print("Server".ljust(15)+": Login...")
+    server.login(username, password)
+    print("Server".ljust(15)+": Login success")
+except Exception as Err:
+    print("Config-Error".ljust(15)+f": {str(Err)}")
 
-    log("Making the EMail structure...")
-    to      = input("\tSend EMail to: ")
-    subject = "Subject: " + input("\tSubject: ") + "\n\n"
-    content = input("\tContent: ")
-    message = subject+content
-    times   = input("\tHow many times [default 1]: ")
-    seconds = input("\tSeconds to wait [default 1]: ")
-    log("All parameters was setted")
-
-    if times == "":
-        times = 1
-    if seconds == "":
-        seconds = 1
-    
-    log("Starting...")
+try:
     for idx, x in enumerate(range(0, int(times)), 1):
         try:
-            print(f"Sending...", end="\r")
-            server.sendmail(usergmail, to, message)
-            print("[%d/%s] EMail was sended to: %s" % (idx, int(times), to))
-            sleep(int(seconds))
+            
+            server.sendmail(username, sender, message)
+            print("[%d/%s] Sended to: %s" % (idx, int(times), sender) + "\r")
+            sleep(int(wait_sec))
         except KeyboardInterrupt:
-            log("Process interrupted by user...")
+            print("\nProcess interrupted by user...")
+            server.quit()
             break
         except Exception as Err:
-            log(f"Error 0x1: {str(Err)}")
-    server.quit()
-    log("Done")
-except Exception as Err:
+            server.quit()
+            print("Spam-Error".ljust(15)+f": {str(Err)}")
     try:
         server.quit()
-    except:
-        pass
-    log(f"Error 0x2: {str(Err)}")
+    except Exception as Err:
+        print("Quit-Error".ljust(15)+f": {str(Err)}")
+except Exception as Err:
+    print("Error".ljust(15)+f": {str(Err)}")
+
+del username
+del password
+exit()
